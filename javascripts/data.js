9b66daf761e93905bc52459d41bf271c407fbafa
gib <gibbowden@gmail.com>
2017-09-18 23:41:37 -0500

    completed initial functions -- just need to add message data to the JSON

---
+++ javascripts/data.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const printToDom = require("./domhandler");
+const loadMessages = require("./xhr");
+
+let messagesArr = [];
+
+//throws error if error on JSON load
+const logError = () => {
+    console.log("You broke me");
+};
+
+//parses JSON data into an array and calls the print to dom function -- runs when the JSON file is loaded 
+const whenMessagesLoad = function () {
+    messagesArr = JSON.parse(this.responseText).messages;
+    printToDom(messagesArr);
+};
+
+//initializer kicks off the XHR --called on main.js
+const initializer = () => {
+  loadMessages(whenMessagesLoad, logError);
+};
+
+//getter for array containing messages in the json file 
+const getInitialMessages = () => {
+    return messagesArr;
+  };
+
+module.exports = {
+    initializer,
+    getInitialMessages
+};
